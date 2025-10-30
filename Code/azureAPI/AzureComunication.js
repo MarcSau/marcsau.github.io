f2c521863162bf36c24bdf0a45da36a47b755556
MarcSau <Marc-Work@DESKTOP-1L66BP0>
2025-10-30 12:05:48 +0100

    [API] Added test connect call and visual representation

--- Code/azureAPI/AzureComunication.js
+++ Code/azureAPI/AzureComunication.js
@@ -1,71 +1,80 @@
-var APIRequest =
-{
-    type: "GET",
-    URL: "",
-    JSON: '{"name":"BlueDabiDi","initialStock":20,"price":17.99,"productType":4}'
+let API_URL = "https://app-storebackend-dev-westeu-01-bmf5hggkhdakhzbx.westeurope-01.azurewebsites.net/";
+let Request_URL = "";
+let outputElement = document.getElementById("output-textbox");
+let lastCachedProducts;
+
+function RequestAllProducts() {
+
+    Request_URL = API_URL + "product";
+
+    fetch(Request_URL)
+        .then(response => {
+            if (!response.ok) {
+                outputElement.innerHTML = response;
+            }
+
+            return response.json();
+        })
+      .then(value => {
+            lastCachedProducts = value;
+            let outputValue = "";
+            console.log("data received")
+            value.forEach(element => {
+                outputValue += GenerateProductText(element);
+                outputValue += "\r\n"
+                console.log(element.name)
+            });
+            outputElement.innerHTML = outputValue;
+      })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-function RequestAllProducts(){
-
-}
-
-function RequestProduct(id){
-
-}
-
-function TryAddProduct(product){
-
-}
-
-function TryDeleteProduct(id){
-
-}
-
-function RequestAllProductTypes(){
-
-}
-
-function TryAddProductType(name){
-
-}
-
-function TryDeleteProductType(id){
+function GenerateHTMLTable(productList){
     
 }
 
-function RequestAllTransactions(){
-    
+function GenerateProductText(product){
+    return product.name + " " + product.price + " " + product.currentStock + " " + product.productType;
 }
 
-function RequestAllTransactions(){
-    
-}
-
-function RequestTransaction(id){
-    
-}
-
-function TryPurchaseProduct(id,amount){
+function RequestProduct(id) {
 
 }
 
-function SendAPIRequest() {
+function TryAddProduct(product) {
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            alert(this.responseText);
-        }
-    };
-    xhttp.open(APIRequest.type, APIRequest.URL, true);
+}
 
-    if (APIRequest.JSON == null) 
-    {
-        xhttp.send();
-    }
-    else 
-    {
-        xhttp.setRequestHeader("Content-type", "application/json");
-        xhttp.send(APIRequest.JSON);
-    }
+function TryDeleteProduct(id) {
+
+}
+
+function RequestAllProductTypes() {
+
+}
+
+function TryAddProductType(name) {
+
+}
+
+function TryDeleteProductType(id) {
+
+}
+
+function RequestAllTransactions() {
+
+}
+
+function RequestAllTransactions() {
+
+}
+
+function RequestTransaction(id) {
+
+}
+
+function TryPurchaseProduct(id, amount) {
+
 }
